c51d3cef4c51988dc4d918fb527ac15d03f17cc4
JetsadaWijit <Ze_ro_owen@hotmail.com>
2024-06-07 09:10:32 +0000

    Set up

---
+++ spigot/src/main/java/com/github/mcengine/MCEngine.java
@@ -0,0 +1,11 @@
+package com.github.mcengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+    @Override
+    public void onDisable() {}
+}
